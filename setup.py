b6096ccdc93757adbc90f6850dbceeff08f53d99
Tommy <tommy@backstit.ch>
2017-07-11 17:34:29 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -93,7 +93,6 @@ include_dirs = [get_python_inc(), pg_config('includedir')]
 library_dirs = [get_python_lib(), pg_config('libdir')]
 define_macros = [('PYGRESQL_VERSION', version)]
 undef_macros = []
-extra_compile_args = ['-O2', '-funsigned-char', '-Wall', '-Werror']
 
 
 class build_pg_ext(build_ext):
